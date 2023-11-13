96ade44140e57c1ba4a426e39ee23cad277b342f
Kaualoha <132154011+808CryptoBeast@users.noreply.github.com>
2023-11-12 15:53:43 -1000

    Update marketanalytics.js

--- marketanalytics.js
+++ marketanalytics.js
@@ -1,10 +1,23 @@
-const body = document.querySelector("body"),
-        sidebar = body.querySelector(".sidebar"),
-        toggle = body.querySelector(".toggle"),
-        searchBtn = body.querySelector(".search-box"),
-        modeSwitch = body.querySelector(".toggle-switch"),
-        modeText = body.querySelector(".mode-text");
+const body = document.querySelector("body");
+const sidebar = document.querySelector(".sidebar");
+const sidebarToggle = document.getElementById("sidebarToggle");
+const searchBtn = body.querySelector(".search-box");
+const modeSwitch = document.querySelector(".toggle-switch");
+const modeText = document.querySelector(".mode-text"); // Define modeText variable
 
-modeSwitch.addEventListener("click", () =>{
+sidebarToggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+});
+searchBtn.addEventListener("click", () => {
+    sidebar.classList.remove("close");
+});
+
+modeSwitch.addEventListener("click", () => {
     body.classList.toggle("dark");
+
+    if (body.classList.contains("dark")) {
+        modeText.innerText = "Light Mode";
+    } else {
+        modeText.innerText = "Dark Mode";
+    }
 });
